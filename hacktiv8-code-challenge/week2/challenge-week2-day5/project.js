1c7998842b9f4066928ab880c07bf85f1d948f16
Danang Aji Tamtomo <imsuretamtomo@gmail.com>
2017-01-04 04:55:50 +0700

    Add project for weeek 2 day 5

---
+++ hacktiv8-code-challenge/week2/challenge-week2-day5/project.js
@@ -0,0 +1,18 @@
+var ClassRoom = function(student) {
+    if(typeof student !== 'object') {
+        return false
+    }
+    var students = []
+    this.student = student
+    this.inputStudent = function() {
+        students.push(this.student);
+    }
+    this.getStudents= function() {
+        return students
+    }
+
+}
+
+var classRoom = new ClassRoom({nama: 'Danang'})
+classRoom.inputStudent()
+console.log(classRoom.getStudents())
