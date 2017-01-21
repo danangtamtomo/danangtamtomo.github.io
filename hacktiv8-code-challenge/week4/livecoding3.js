3f47d7eee512eeac1a803bf991319ec3a4f6df87
Danang Aji Tamtomo <imsuretamtomo@gmail.com>
2017-01-21 20:37:44 +0700

    Update livecoding3.js

--- hacktiv8-code-challenge/week4/livecoding3.js
+++ hacktiv8-code-challenge/week4/livecoding3.js
@@ -2,32 +2,28 @@ function BreakNameCode(code) {
   var splitCode = code.split('');
   var posOfCode = [];
   var codeSequence = ['C','O','D','E'];
-  // var matchCode = code.match(/[CODE]/g);
-  // if(/[CODE]/g.test(code)) return "Wrong Code";
   var eva = splitCode.slice();
+  var limiter = 0;
   for(var i=0;i<codeSequence.length;i++) {
-    for (var j = 0; j < eva.length; j++) {
-      // console.log(splitCode[j] === codeSequence[i]);
-        // console.log(splitCode[j],splitCode[j] === codeSequence[i] && typeof posOfCode[i] === 'undefined' );
-      if(eva[j] === codeSequence[i] && typeof posOfCode[i] === 'undefined' ) {
-        posOfCode.push([eva[j], code.indexOf(eva[j])]);
-        eva = eva.slice(code.indexOf(splitCode[j]), splitCode.length);
-        console.log(code.indexOf(splitCode[j]));
-        // console.log(code.indexOf(splitCode[j]),splitCode.length);
-        console.log(eva.toString());
+    var isThere = false;
+    for (var j = limiter; j < splitCode.length; j++) {
+      if(splitCode[j] === codeSequence[i] && typeof posOfCode[i] === 'undefined' ) {
+        posOfCode.push([splitCode[j], j]);
+        limiter = j;
+        isThere = true;
       }
     }
+    if(isThere === false) return false;
   }
 
   var result = "";
   for (var k = 0; k < posOfCode.length; k++) {
     if(k === posOfCode.length-1) {
-      result += codeSequence[k]+code.substr(posOfCode[k][1]+1, code.length-posOfCode[k][1]).toLowerCase();
+      result += code.charAt(posOfCode[k][1])+code.substr(posOfCode[k][1]+1, code.length-posOfCode[k][1]).toLowerCase();
     } else {
-      result += codeSequence[k]+code.substr(posOfCode[k][1]+1, posOfCode[k+1][1]-(posOfCode[k][1]+1)).toLowerCase()+' ';
+      result += code.charAt(posOfCode[k][1])+code.substr(posOfCode[k][1]+1, posOfCode[k+1][1]-(posOfCode[k][1]+1)).toLowerCase()+' ';
     }
   }
-  console.log(posOfCode);
   return result;
 }
 
